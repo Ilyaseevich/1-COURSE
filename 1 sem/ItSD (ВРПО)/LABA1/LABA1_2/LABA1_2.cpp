3e3fb8a18810fafa8e2496aad33f5d73046146e8
Роман <ilyaseeviiich@gmail.com>
2024-12-24 19:12:55 +0300

    Add files via upload

---
+++ 1 sem/ItSD (ВРПО)/LABA1/LABA1_2/LABA1_2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	char hello[] = "Hello, ";
+	char fio[] = "Ilyaseevich Roman Sergeevich";
+	cout << hello << fio << "\n";
+
+	return 0;
+}
